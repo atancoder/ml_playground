9d221bbba702dad2d00e1b7c08d19e719c141d1c
Anthony Tan <atan5133@gmail.com>
2023-05-24 21:32:21 -0700

    uncomment regression models

--- regression/quadratic_regression.py
+++ regression/quadratic_regression.py
@@ -27,9 +27,7 @@ test_data, test_labels = generate_train_data()
 
 data_args = (training_data, training_labels, test_data, test_labels)
 
-# linear_regression_model(*data_args)
+linear_regression_model(*data_args)
 quadratic_regression_model(*data_args)
-# gaussian_regression_model(
-#     *data_args, gamma=1
-# )
+gaussian_regression_model(*data_args, gamma=1)
 gaussian_regularization_regression_model(*data_args, gamma=1, alpha=0.01)
